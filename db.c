d9dc29c4ef36bf273f1a7b91175a935a6d5547c3
Erik F <erfr8032@celsius.it.uu.se>
2016-10-07 17:59:23 +0200

    initial commit

---
+++ db.c
@@ -0,0 +1,293 @@
+#include "utils.h"
+#include "list.h"
+#include "tree.h"
+
+
+void print_item(item_t *item)
+{
+  printf("Name:%s\nDescription:%s\nPrice:%d.%d\n", item->name, item->description, item->price, item->price/100);
+  for (int i = 0; i < list_length(item->shelf); i++)
+    {
+      printf(("%s, %d") item->shelf[i]->shelfname, item->shelf->amount);
+    }
+}
+
+item_t make_item(char *name, char *description, int price, shelf_t *shelf)
+{
+  item_t createditem;
+  createditem->name = name;
+  createditem->description= description;
+  createditem.price = price;
+  shelf_t shelves = list_new();
+  for (int i = 0; i < list_length(shelves) ; i++)
+    {
+      list_insert(shelves, i,shelves[i]->value);
+    }
+  return createditem;
+  ///Nagon listfunktion for att ta in alla shelves?
+}
+
+bool is_shelf(shelf_t *shelf)
+{
+  if ((isdigit(shelf->shelfname[0])))
+    {
+      return false;
+    }
+  for (int i = 1; i < strlen(shelf->shelfname); i++)
+    {
+      if (!isdigit(shelf->shelfname[i]))
+	{
+	  return false;
+	}
+    }
+  return true;
+}
+  
+shelf_t ask_question_shelf(char *question)
+{
+  return ask_question(question, is_shelf ,(convert_func) strdup).s;
+}
+
+
+item_t input_item()
+{
+  char *name;
+  char *description;
+  int price;
+  shelf_t shelf;
+  name = ask_question_string("Vad heter varan?");
+  description = ask_question_string("Vad ar varans beskrivning?");
+  price = ask_question_int("Vad ar varans pris?");
+  shelf = ask_question_shelf("Vilken hyllplats ar varan pa?");
+  item_t item = (make_item(name, description, price, shelf));
+  return item;
+  ///tree_insert bor anvandas har
+}
+
+char * magick(char *firstarray[], char *secondarray[], char *thirdarray[], int lengthofarray)
+{
+  int buf_siz = 255;
+  char buf[buf_siz];
+  int index = 0;
+  char c;
+  int i = 0;
+  char *firstword = firstarray[rand() % lengthofarray];
+  char *secondword = secondarray[rand() % lengthofarray];
+  char *thirdword = thirdarray[rand() % lengthofarray];
+ 
+  for(i = 0; i <= strlen(firstword) ; ++i )
+    {
+      c = firstword[i];
+      buf[index] = c;
+      if (c == '\0')
+	{
+	  buf[index] = '-';
+	}
+      ++index;
+    }
+  for(i = 0 ; i <= strlen(secondword) ; ++i)
+    {
+      c = secondword[i];
+      buf[index] = c;
+      if (c == '\0')
+	{
+	  buf[index] = ' ';
+	}
+      ++index;
+    }
+  for(i = 0 ;i <= strlen(thirdword); ++i )
+    {
+      c = thirdword[i];
+      buf[index] = c;
+      if (c == '\0')
+	{
+	  buf[index] = '\0';
+	}
+      ++index;
+    }
+   
+  return strdup(buf);
+}
+
+
+
+void list_db(item_t *items, int no_items)
+{
+  for(int i = 0; i <  no_items; ++i)
+    {
+      printf("%d. %s\n",i+1, items[i].name);
+    }
+}
+
+
+item_t *edit_db(item_t *db, int db_siz)
+{
+  list_db(db, db_siz); //Lists database
+  int pos = ask_question_int("\nVilken vara skall editeras?");
+  while (pos > 16 || pos<1)
+    {
+      pos = ask_question_int("Du måste välja en vara mellan 1-16!\nVilken vara skall editeras?");
+    }
+  print_item(&db[pos-1]);  //Varför '&'???
+  printf("\nVill du ersätta vara %d med en ny vara?", pos);
+  char *choice =  ask_question_string("(Y/N)");
+  if (*choice == 'Y' || *choice == 'y')
+    {
+      db[pos-1] = input_item();
+      puts("\n");
+      return db;
+    }
+  puts("\n");
+  return db;
+}
+
+void add_item_to_db(item_t *db, int *db_siz)
+{
+  item_t newitem = input_item();
+  db[*db_siz] = newitem;
+  *db_siz = *db_siz + 1;
+}
+
+void remove_item_from_db(item_t *db, int *db_siz)
+{
+  list_db(db, *db_siz);
+  int pos = ask_question_int("\nVilken vara skall tas bort?");
+  while (pos > 16 || pos<1)
+    {
+      pos = ask_question_int("Du måste välja en vara mellan 1-16!\nVilken vara skall editeras?");
+    }
+  for ( int index = pos; 16 > index ; ++index)
+    {
+      db[index-1] = db[index];
+    }
+  *db_siz = *db_siz-1;
+}
+
+void print_menu()
+{
+  puts("\n[L]ägga till en vara\n");
+  puts("[T]a bort en vara\n");
+  puts("[R]edigera en vara\n");
+  puts("Ån[g]ra senaste ändringen\n");
+  puts("Lista [h]ela varukatalogen\n");
+  puts("[A]vsluta");
+}
+
+char ask_question_menu()
+{
+  print_menu();
+  char *answer = ask_question("\nValj ett alternativ\n", not_empty, (convert_func) strdup).s;
+  char uppercaseanswer = (toupper(answer[0]));
+  if (uppercaseanswer == 'L' || 'T' || 'R' || 'G' || 'H' || 'A')
+    {
+      return uppercaseanswer;
+    }
+  else
+    {
+      puts("Invalid input!");
+      ask_question_menu();
+    }
+}
+
+void event_loop (item_t *db, int *db_siz)
+{
+  char choice = ask_question_menu();
+  int control = 0;
+  while( control == 0)
+    {
+      if (choice == 'L')
+        {
+          if (*db_siz >= 16)
+            {
+              puts("For manga element!");
+              choice = ask_question_menu();
+            }
+          else
+            {
+	      add_item_to_db(db, db_siz);
+	      choice = ask_question_menu();
+            }
+        }
+      else if (choice == 'T')
+        {
+          remove_item_from_db(db, db_siz);
+          choice = ask_question_menu();
+        }
+      else if (choice == 'R')
+        {
+          edit_db(db, *db_siz);
+          choice = ask_question_menu();
+        }
+      else if (choice == 'G')
+        {
+          puts("Not yet implemented");
+          choice = ask_question_menu();
+        }
+      else if (choice == 'H')
+        {
+          list_db(&db[0], *db_siz);
+          choice = ask_question_menu();
+        }
+      else if (choice == 'A')
+        {
+          control = control + 1;
+        }
+      else
+        {
+          puts("Error with character check");
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+  char *array1[] = { "Laser",        "Polka",    "Extra" };
+  char *array2[] = { "förnicklad",   "smakande", "ordinär" };
+  char *array3[] = { "skruvdragare", "kola",     "uppgift" };
+  if (argc < 2)
+    {
+      printf("Usage: %s number\n", argv[0]);
+    }
+  else
+    {
+      item_t db[16]; // Array med plats för 16 varor
+      int db_siz = 0;    // Antalet varor i arrayen just nu
+
+      int items = atoi(argv[1]); // Antalet varor som skall skapas
+
+      if (items > 0 && items <= 16)
+	{
+	  for (int i = 0; i < items; ++i)
+	    {
+	      // Läs in en vara, lägg till den i arrayen, öka storleksräknaren
+	      item_t item = input_item();
+	      db[db_siz] = item;
+	      ++db_siz;
+	    }
+	}
+      else    {
+	puts("Sorry, must have [1-16] items in database.");
+	return 1; // Avslutar programmet!
+      }
+
+      for (int i = db_siz; i < 16; ++i)
+	{
+	  char *name = magick(array1, array2, array3, 3); /// Lägg till storlek
+	  char *desc = magick(array1, array2, array3, 3); /// Lägg till storlek
+	  int price = rand() % 200000;
+	  char shelf[] = { rand() % ('Z'-'A') + 'A',
+			   rand() % 10 + '0',
+			   rand() % 10 + '0',
+			   '\0' };
+	  item_t item = make_item(name, desc, price, strdup(shelf));
+
+	  db[db_siz] = item;
+	  ++db_siz;
+	}
+
+      // Skriv ut innehållet
+      list_db(&db[0], db_siz);
+      event_loop(db, &db_siz);
+    }
+  return 0;
+}
---
+++ list.c
@@ -0,0 +1,398 @@
+/// !! OBS -- du kommer att behöva ändra lite i denna fil. 
+/// !! Detta är en lista som endast hanterar heltal. Men det 
+/// !! kommer inte att räcka för oss här eftersom vi skall ha
+/// !! andra data (vilka??) i vår (enda) lista.
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+/// Define struct list in your .c file not here! (why?)
+typedef struct list list_t;
+
+typedef struct link link_t;
+
+struct link
+{
+  int value;
+  link_t *next;
+};
+
+struct list
+{
+  link_t *first;
+  link_t *last; 
+};
+
+/// \file list.h
+///
+/// \author Tobias Wrigstad
+/// \version 1.0
+/// \date 2015-08-28
+/// \bug None known. But this code is untested so there are likely bigs in it. 
+
+
+/// Creates a new list
+///
+/// \returns: empty list
+list_t *list_new()
+{
+  list_t *createdlist = malloc(sizeof(list_t));
+  createdlist->first = NULL;
+  createdlist->last = NULL;
+  return createdlist;
+}
+
+
+link_t *link_new()
+{
+  link_t *createdlink = malloc(sizeof(link_t));
+
+  createdlink->value = NULL;
+  createdlink->next = NULL;  
+
+  return createdlink;
+  
+}
+
+/// Inserts a new element at the end of the list
+///
+/// \param list pointer to the list
+/// \param elem the integer to be appended
+void list_append(list_t *list, int elem)
+{
+  
+  link_t *new_l = link_new(); 
+  new_l->value = elem;
+  
+
+  if (list->last == 0){
+    
+    list->last = new_l;
+    list->first= new_l;
+  }
+
+  else {
+    list->last->next = new_l;
+    
+  }
+  
+  
+  
+}
+/// Inserts a new element at the beginning of the list
+///
+/// \param list pointer to the list
+/// \param elem the integer to be prepended
+
+void list_prepend(list_t *list, int elem)
+{
+  
+  link_t *new_l = link_new();
+  new_l->value = elem; 
+
+
+  if (list->last == 0){
+    
+    list->last = new_l;
+    list->first= new_l;
+  }
+
+  else {
+    new_l->next = list->first;
+    list->first = new_l;
+    }   
+}
+
+/// Inserts a new element at a given index. 
+///
+/// Valid indexes are [0..size]. 
+///
+/// Example:
+///
+/// list_t *l = list_new(); // l == []
+/// list_insert(l, 0, 42);  // l == [42]
+/// list_insert(l, 0, 43);  // l == [43, 42]
+/// list_insert(l, 1, 44);  // l == [43, 44, 42]
+/// list_insert(l, 5, 45);  // l == [43, 44, 42]
+///
+/// The last case fails (and returns false) because
+/// size is 3, meaning 5 is not a valid index. 
+///
+/// Note that insert at index 0 is the same as prepend 
+/// and insert index size is the same as append. 
+///
+/// Negative indexes should be supported:
+///
+/// list_insert(l, -1, 45);       // l == [43, 44, 42, 45]
+///
+/// A positive index can be calculated from a 
+/// negative like this: pos_i = size + 1 + neg_i.
+/// 
+/// \param list  pointer to the list
+/// \param index the index for elem to be inserted at
+/// \param elem  the integer to be prepended
+/// \returns true if succeeded, else false
+bool list_insert(list_t *list, int index, int elem)
+{
+ 
+  struct link *travlist;
+  struct link *travlist_prev = 0;
+  
+     
+  
+      
+      if (list->first == 0) {
+
+	if (index != 0) {
+	  puts("ej tillåtet index");
+	  return false;
+	}
+
+	 link_t *new = link_new();
+	 new->value = elem;  
+	 list->first = new;
+	 list->last = new;
+	 return true;
+      }
+
+
+
+      
+      travlist = list->first;
+      
+
+
+
+      for(int i = 0; i < index; i++)
+	{
+
+	 
+	  if (travlist->next == 0) {
+	     puts("ej tillåtet index");
+	       return false;
+	}
+	  travlist_prev = travlist;
+	  travlist = travlist->next;
+	 
+	}
+      
+      
+
+      //}
+       
+      
+      
+     
+
+      link_t *new = link_new();
+      new->value = elem;    
+      new->next = travlist;
+      
+     
+      if (travlist_prev != 0) travlist_prev->next = new;
+ 
+      
+      if (index == 0)
+	{
+	  list->first = new;
+	}
+      return true;
+      
+  
+}
+// haer legger vi in not
+
+
+/// Removes an element from a list.
+///
+/// Example: (assume l == [43, 44, 42, 45])
+///
+/// int elem;
+/// list_remove(l, 1, &elem);  // l = [43, 42, 45], elem == 44
+/// list_remove(l, -1, &elem); // l = [43, 42], elem == 45
+///
+/// \param list  pointer to the list
+/// \param index the index to be removed
+/// \param elem a pointer to where the element can be stored
+/// \returns true if succeeded, else 
+bool list_remove(list_t *list, int index, int *elem)
+{
+  struct link *travlist;
+  //struct link *travlist_prev;
+  if (list->first != 0)
+    {
+      travlist = list->first;
+      for(int i = 0; (i = index || travlist->next == 0); i++)
+	{
+	  //travlist_prev = travlist;
+	  travlist = travlist->next;
+	}
+      if (travlist == 0) return false; 
+      else
+	{
+	  elem = &travlist->value;
+	  //travlist_prev = travlist->next;
+	  return true;
+	}
+    }
+  return false;
+}
+
+/// Returns the element at a given index
+/// \param list  pointer to the list
+/// \param index the index to be returns
+/// \returns a pointer to the element at index index
+int *list_get(list_t *list, int index)
+{
+  int i = 0;
+  int *value = 0;
+  struct link *travlist;
+
+  travlist = list->first;
+  
+  for(;(i < index && travlist->next != 0); i++)
+    {
+      travlist = travlist->next;
+    }
+  value = &travlist->value; 
+  return value;	
+}
+
+/// Returns the length of the list. It is undefined
+/// whether the length is calculated in O(n) time or
+/// whether there is a size counter in the list object
+/// that is manipulated by insert, remove, etc. 
+/// \param list the list
+/// \returns the length of list
+int list_length(list_t *list)
+{
+  
+  int i = 0;
+  struct link *travlist;
+  if (list->first != 0)
+    {
+
+      travlist = list->first;
+      for(i = 1; travlist->next != 0; i++)
+	{
+	  	  travlist = travlist->next;
+	}
+      
+      return i;
+      
+    }
+
+      return 0; 
+}
+
+/// A convenience for list_get(list, 0)
+int *list_first(list_t *list)
+{  
+  return (list_get(list, 0));
+}
+
+/// A convenience for list_get(list, -1)
+int *list_last(list_t *list)
+{
+  return (list_get(list, list_length(list)));
+}
+
+
+void clear(void) {
+ int u;
+  do {
+    u = getchar();
+  }
+  while (u != '\n');
+}
+
+int main()
+{
+  list_t *list = list_new();
+  
+  int ind;
+  int val;
+  char c;
+  
+  
+  while (true) {
+    int len = list_length(list);
+    printf("Langden av listan: %d\n", len);
+    puts(" [a]dd index value\n app[e]nd value\n [p]repend value\n [g]et index value\n [f]irst element in list\n [l]ast element in list");
+        
+    scanf("%c", &c);
+    switch (c) {
+      
+    case 'a':
+      clear();
+      
+      scanf("%d %d", &ind, &val);
+      printf("index: %d\nvalue: %d\n",ind,val);
+      
+        
+      list_insert(list , ind, val);
+      clear();
+      break;
+      
+      
+    case 'g': 
+      clear();
+      puts("vilket index vill du visa value i:");
+      scanf("%d", &ind);
+      
+      
+      if (len < ind || len == 0) printf("det finns inget element vid index %d\n",ind);
+      else {
+	int *index_value_get = list_get(list, ind);
+	printf("index %d elementet i listan e: %d\n",ind, *index_value_get);
+      }
+      clear();
+      break;
+ 
+
+    case 'f':
+      clear();
+      if (len == 0) puts("listan e tom, det finns inget first element");
+      else {
+	int *first = list_first(list);
+	printf("Forsta elementet i listan ar: %d\n", *first);
+      }      
+      break;
+  
+    case 'l':
+      clear();
+      if (len == 0) puts("listan e tom, det finns inget last element");
+      else {
+	int *last = list_last(list);
+	printf("sista elementet i listan e: %d\n", *last);
+      }
+      break;
+      
+    case 'e':
+      clear();
+      puts("vilket value vill du appenda:");
+      scanf("%d", &ind);
+      
+      list_append(list, ind);
+      int *last = list_last(list);
+      printf("sista elementet i listan ar nu: %d\n", *last);
+            
+      clear();
+      break;
+
+      
+    case 'p':
+      
+      clear();
+      puts("vilket value vill du appenda:");
+      scanf("%d", &ind);
+      
+      list_prepend(list, ind);
+      int *first = list_first(list);
+      printf("forsta elementet i listan ar nu: %d\n", *first);
+      clear();
+      break;
+      
+    }
+  }
+return 0;
+}
